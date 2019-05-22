0ed15c67f78d6488ed5b349a803ca75de568a67c
Stephen <stephen.schaal@gmail.com>
2019-05-21 20:51:02 -0400

    stuff

--- src/app/routes/admin/photos/photos.component.ts
+++ src/app/routes/admin/photos/photos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore'
 import { Photo } from 'src/app/models/photo'
 import { Observable } from 'rxjs'
+import { AngularFireStorage } from '@angular/fire/storage'
 
 @Component({
   selector: 'app-photos',
@@ -13,7 +14,9 @@ export class PhotosComponent implements OnInit {
   private photosCollection: AngularFirestoreCollection<Photo>
   photos$: Observable<Photo[]>
 
-  constructor(private afs: AngularFirestore) {
+  constructor(
+    private afs: AngularFirestore,
+    private afStorage: AngularFireStorage) {
   }
 
   ngOnInit() {
@@ -21,4 +24,12 @@ export class PhotosComponent implements OnInit {
     this.photos$ = this.photosCollection.valueChanges()
   }
 
+  removePhoto(photo: Photo) {
+    this.afStorage.ref('/uploads/' + photo.uid + '/' + photo.fileName).delete()
+    this.photosCollection.doc(photo.id).delete()
+  }
+
+  handleClick(photo: Photo) {
+    this.removePhoto(photo)
+  }
 }
